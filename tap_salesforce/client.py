c51782356bf3239a195b02f05bb23c8b103e9eb8
Lior Ben-David <liorbd@Outlook.com>
2026-01-30 17:05:44 -0500

    HGI-9610 | use linear page count reduction (#4)
    * use linear page count reduction
    
    * Adjust numbers

--- tap_salesforce/client.py
+++ tap_salesforce/client.py
@@ -249,9 +249,10 @@ class SalesforceStream(RESTStream):
         ):
             msg = self.response_error_message(response)
             count = self.config.get("order_page_size") if hasattr(self.config,"order_page_size") else self.count if hasattr(self,"count") else 200
-            if count > 4:
-                self.logger.debug(f"Hit 50x error, automatically reducing count from {count} to {int(count / 2)}")
-                self.count = int(count / 2)
+            if count > 40:
+                self.count = count - 20
+                self.logger.debug(f"Hit 50x error, automatically reducing count from {count} to {self.count}")
+
             raise RetriableAPIError(msg, response)
         try:
             res_json = response.json()
